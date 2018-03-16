5e8ea63c39d3bfd08840fc2231c9d611d429c263
Fernvaz <35124856+Fernvaz@users.noreply.github.com>
2018-03-15 18:07:37 -0700

    Update create_ldap.py
    Made changes at the bottom of the code

--- create_ldap.py
+++ create_ldap.py
@@ -65,4 +65,7 @@ def create_instance(compute, project, zone, name):
     }
 
 
-return compute.instances().insert(project='my-first-project', zone='us-west1-a', body=config).execute()
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
